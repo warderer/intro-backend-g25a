e1ae7b2d953967338d2e4f68780ff50341b034cf
Francisco Campos <sasasasamaes13@gmail.com>
2023-10-30 20:35:44 -0600

    query params

--- 01-intro-express/api/v1/pets.js
+++ 01-intro-express/api/v1/pets.js
@@ -5,8 +5,22 @@ const router = express.Router()
 const petList = require('../../petsData.js')
 
 // Crear la ruta GET /pets
+//Query params : ?key=value&key2=value2
+//Son similares a los params,  pero se suelen  enviar en la url para enviar uno o mas datos
+// las querys son aviertas, no definimos cuantas variables nos tienen que mandar, ni como se llaman, nuestra responsabilidad
+// es solo tomar en cuanta aquellos que nos intereses
 router.get('/api/v1/pets', (request, response) => {
   console.log('Query params', request.query)
+  const { age, type } = request.query
+  if(!age && !type) {
+     response.status(200).send(petList)
+     return
+  }
+  const filteredPets = petList.pets.filter((pet) => {
+    return pet.age = age || pet.type === type
+  })
+  response.status(200).send(filteredPets)
+  return
 })
 
 module.exports = router
